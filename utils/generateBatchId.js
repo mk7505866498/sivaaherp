77c2051adb0b8e0df3f8080e65fc45336f9e8e9a
mk2429 <mayank.20429@knit.ac.in>
2026-05-24 15:39:59 +0530

    first5

--- utils/generateBatchId.js
+++ utils/generateBatchId.js
@@ -3,11 +3,27 @@ export default function generateBatchId(
   silverRate,
   labourPerGram
 ) {
+
   const d = new Date(date);
 
-  const day = String(d.getDate()).padStart(2, "0");
-  const month = String(d.getMonth() + 1).padStart(2, "0");
-  const year = d.getFullYear();
+  const day =
+    String(d.getDate()).padStart(2, "0");
 
-  return `${day}${month}${year}${silverRate}${labourPerGram}`;
+  const month =
+    String(d.getMonth() + 1).padStart(2, "0");
+
+  const year =
+    d.getFullYear();
+
+  // RATE ALWAYS 3 DIGIT
+
+  const formattedRate =
+    String(silverRate).padStart(3, "0");
+
+  // LABOUR ALWAYS 3 DIGIT
+
+  const formattedLabour =
+    String(labourPerGram).padStart(3, "0");
+
+  return `${day}${month}${year}${formattedRate}${formattedLabour}`;
 }
